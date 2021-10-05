1f034edb857d7da262fb635f0c1180a3983d199a
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-05 16:34:07 -0600

    Create validation.mjs

---
+++ validation.mjs
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Unlicense
+ *
+ * This file is part of Clone All Repos.
+ *
+ * This source code is licensed under the Unlicense License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/unlicense.
+ */
