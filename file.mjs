4fb3c402998668161cc7ced7fc455ad1ec4b17a5
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-10-05 18:01:09 -0600

    Check clone directory existance

--- file.mjs
+++ file.mjs
@@ -7,3 +7,18 @@
  * LICENSE file in the root directory of this source tree or at
  * https://opensource.org/licenses/unlicense.
  */
+
+import fs from 'fs';
+import path from 'path';
+
+export function checkCloneDir(relDir) {
+  const absPath = path.resolve(relDir);
+
+  mkdirIfNotExists(absPath);
+}
+
+function mkdirIfNotExists(dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+}
